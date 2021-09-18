74f897e1c477acd8fac076472988c1a108ffef63
Arturo Arroyo <turo310@gmail.com>
2021-09-18 10:51:09 -0700

    Pages and routing

---
+++ pages/sponsors/add.tsx
@@ -0,0 +1,7 @@
+export default function AddSponsor() {
+    return (
+        <div>
+            <h1>Add Sponsor</h1>
+        </div>
+    )
+}
---
+++ pages/sponsors/index.tsx
@@ -0,0 +1,7 @@
+export default function index() {
+    return (
+        <div>
+            <h1>Sponsors</h1>
+        </div>
+    )
+}
