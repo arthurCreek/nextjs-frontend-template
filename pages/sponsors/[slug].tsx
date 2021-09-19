259ff53d0d1104bb676936da9cff35cd2f9d68dc
Arturo Arroyo <turo310@gmail.com>
2021-09-19 10:58:47 -0700

    Fetch server side props for single sponsor page

--- pages/sponsors/[slug].tsx
+++ pages/sponsors/[slug].tsx
@@ -1,11 +1,66 @@
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '@/components/Layout';
+import {API_URL} from '@/config/index';
+import styles from '@/styles/Event.module.css';
 
+export interface Props {
+    spons: any
+}
 
-export default function SponsorPage() {
+export default function SponsorPage(props: Props) {
+    const deleteEvent = (e: any) => {
+        console.log('Delete');
+    }
 
     return (
         <Layout>
-            <h1>Sponsor</h1>
+            <div className={styles.event}>
+                <div className={styles.controls}>
+                    <Link href={`/sponsors/edit/${props.spons.id}`}>
+                        <a>
+                            <FaPencilAlt /> Edit Sponsor
+                        </a>
+                    </Link>
+                    <a href="#" className={styles.delete} onClick={deleteEvent}>
+                        <FaTimes /> Delete Event
+                    </a>
+                </div>
+
+                <h1>{props.spons.name}</h1>
+                {props.spons.image && (
+                    <div className={styles.image}>
+                        <Image alt="Sponsor" src={props.spons.image} width={960} height={600} />
+                    </div>
+                )}
+
+                <h3>Description:</h3>
+                <p>{props.spons.description}</p>
+
+                <Link href='/sponsors'>
+                    <a className={styles.back}>
+                        {'<'} Go Back
+                    </a>
+                </Link>
+            </div>
         </Layout>
     )
+}
+
+export interface ServerProp {
+    query: {
+        slug: any
+    }
+}
+
+export async function getServerSideProps(props: ServerProp) {
+    const res = await fetch(`${API_URL}/api/sponsors/${props.query.slug}`);
+    const sponsors = await res.json();
+
+  return {
+    props: {
+        spons: sponsors[0]
+    }
+  }
 }
