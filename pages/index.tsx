9a1cdea8e3c2695e8ae701cfca41be2be7e365d8
Arturo Arroyo <turo310@gmail.com>
2021-09-19 10:06:16 -0700

    Add server side props to fetch data in index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 import Layout from '@/components/Layout';
+import {API_URL} from '@/config/index';
 
-export default function HomePage() {
+export interface Props {
+  sponsors: any
+}
+
+export default function HomePage(props: Props) {
+  console.log(props.sponsors);
+
   return (
       <Layout>
           <h1>Home</h1>
       </Layout>
   )
+}
+
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/sponsors`);
+  const sponsors = await res.json();
+
+  return {
+    props: {sponsors}
+  }
 }
