9053d8702fdc6e489c5cafa7ca8ca1cdc37f5d39
Arturo Arroyo <turo310@gmail.com>
2021-09-25 14:57:42 -0700

    Get user sponsors for dashboard

--- pages/account/dashboard.tsx
+++ pages/account/dashboard.tsx
@@ -1,9 +1,39 @@
+import { parseCookies } from "../../helpers/index";
 import Layout from "@/components/Layout"
+import { API_URL } from "@/config/index";
 
-export default function DashboardPage() {
+export interface Props {
+    sponsors: any
+}
+
+export default function DashboardPage(props: Props) {
+    console.log(props.sponsors);
     return (
         <Layout title='User Dashboard'>
             <h1>Dashboard</h1>
         </Layout>
     )
+}
+
+export interface ServerProp {
+    req: any
+}
+
+export async function getServerSideProps(serverProps: ServerProp) {
+    const {token} = parseCookies(serverProps.req);
+
+    const res = await fetch(`${API_URL}/sponsors/me`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    const sponsors = await res.json();
+
+    return {
+        props: {
+            sponsors
+        }
+    }
 }
